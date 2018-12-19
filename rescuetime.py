c5d29389fcfb2cf234068c2456e7a227f8c56e02
Matthew Jorgensen <matthew@jrgnsn.net>
2018-12-19 01:02:36 -0600

    Added .gitignore

--- rescuetime.py
+++ rescuetime.py
@@ -7,6 +7,15 @@ class RescueAPI():
     def __init__(self, ):
         self.API_KEY = os.environ['RESCUETIME_API_KEY']
 
+    def read_highlights(self, start_date=None, end_date=None):
+        ENDPOINT = "http://www.rescuetime.com/anapi/highlights_feed"
+        payload = {}
+        payload['key'] = self.API_KEY
+        if start_date: payload['start_date'] = start_date
+        if end_date: payload['end_date'] = end_date
+        r = requests.get(ENDPOINT, params=payload)
+        return {'status_code': r.status_code, 'json': r.json()}
+
     def post_highlight(self, message, highlight_date=None, source=None):
         ENDPOINT = "https://www.rescuetime.com/anapi/highlights_post"
         now = datetime.date.today()
@@ -16,4 +25,4 @@ class RescueAPI():
         payload['description'] = message
         if source: payload['source'] = source
         r = requests.post(ENDPOINT, params=payload)
-        return {'status_code': r.status_code,'json': r.json()}
+        return {'status_code': r.status_code, 'json': r.json()}
--- run.py
+++ run.py
@@ -2,11 +2,26 @@ import json
 import rescuetime
 
 rt = rescuetime.RescueAPI()
+
+print("""
+--------------------------------------------------------------------------------
+Testing Highlights Feed
+--------------------------------------------------------------------------------
+""")
+result = rt.read_highlights()
+print(result['status_code'])
+payload = result['json']
+
+print("""
+--------------------------------------------------------------------------------
+Testing Highlights POST
+--------------------------------------------------------------------------------
+""")
 result = rt.post_highlight("Test")
-json = json.dumps(result['json'], indent=2)
+payload = result['json']
 if result['status_code'] == 200:
     print('Success!')
-    print(json)
+    print(payload)
 elif result['status_code'] == 400:
     print('Fail!')
-    print(json)
+    print(payload)
