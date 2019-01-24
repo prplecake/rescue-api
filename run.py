3a775b1b1769e1b68c23211bd567cfc920042ae5
Matthew Jorgensen <matthew@jrgnsn.net>
2019-01-23 23:42:54 -0600

    Added a test for the alerts feed function and the alert status function.

--- run.py
+++ run.py
@@ -5,10 +5,30 @@ rt = rescuetime.RescueAPI()
 
 print("""
 --------------------------------------------------------------------------------
+Testing Alerts Feed
+--------------------------------------------------------------------------------
+""")
+result = rt.list_alerts()
+print(result['status_code'])
+payload = json.dumps(result['json'], indent=2)
+print(payload)
+
+print("""
+--------------------------------------------------------------------------------
+Testing Alerts Status
+--------------------------------------------------------------------------------
+""")
+result = rt.alert_status()
+print(result['status_code'])
+payload = json.dumps(result['json'], indent=2)
+print(payload)
+
+print("""
+--------------------------------------------------------------------------------
 Testing Highlights Feed
 --------------------------------------------------------------------------------
 """)
-result = rt.read_highlights(end_date="yesterday")
+result = rt.read_highlights(end_date="2019-01-23")
 print(result['status_code'])
 payload = json.dumps(result['json'], indent=2)
 print(payload)
