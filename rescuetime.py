9c32b7644437963b166719292e37d5a81bdd46e2
Matthew Jorgensen <matthew@jrgnsn.net>
2019-01-23 23:42:13 -0600

    Linted and added a function
    - Followed PEP8 and moved multiple expressions to multiple lines
    - Created `alert_status` and `list_alerts` functions.

--- rescuetime.py
+++ rescuetime.py
@@ -8,12 +8,31 @@ class RescueAPI:
     def __init__(self, ):
         self.API_KEY = os.environ['RESCUETIME_API_KEY']
 
-    def read_highlights(self, start_date=None, end_date=None):
-        ENDPOINT = "http://www.rescuetime.com/anapi/highlights_feed"
+    def alert_status(self, alert_id=None):
+        ENDPOINT = "https://www.rescuetime.com/anapi/alerts_feed"
         payload = {}
         payload['key'] = self.API_KEY
-        if start_date: payload['start_date'] = start_date
-        if end_date: payload['end_date'] = end_date
+        payload['op'] = 'status'
+        payload['alert_id'] = alert_id
+        r = requests.get(ENDPOINT, params=payload)
+        return {'status_code': r.status_code, 'json': r.json()}
+
+    def list_alerts(self,):
+        ENDPOINT = "https://www.rescuetime.com/anapi/alerts_feed"
+        payload = {}
+        payload['key'] = self.API_KEY
+        payload['op'] = 'list'
+        r = requests.get(ENDPOINT, params=payload)
+        return {'status_code': r.status_code, 'json': r.json()}
+
+    def read_highlights(self, start_date=None, end_date=None):
+        ENDPOINT = "https://www.rescuetime.com/anapi/highlights_feed"
+        payload = {}
+        payload['key'] = self.API_KEY
+        if start_date:
+            payload['start_date'] = start_date
+        if end_date:
+            payload['end_date'] = end_date
         r = requests.get(ENDPOINT, params=payload)
         return {'status_code': r.status_code, 'json': r.json()}
 
@@ -24,6 +43,7 @@ class RescueAPI:
         payload['key'] = self.API_KEY
         payload['highlight_date'] = highlight_date if highlight_date else now
         payload['description'] = message
-        if source: payload['source'] = source
+        if source:
+            self.payload['source'] = source
         r = requests.post(ENDPOINT, params=payload)
         return {'status_code': r.status_code, 'json': r.json()}
